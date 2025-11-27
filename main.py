fbb6bc6da69b930db855624170ddab63faab15b2
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2025-11-27 20:06:15 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+import time
+
+names = [
+    "ნუკი გაგნიძე",
+    "გიორგი ყორანაშვილი",
+    "იოანე ჯიშკარიანი",
+    "თიკა ქათამაძე",
+    "ლილე ძნელაძე",
+    "ანდრია მაღლაფერიძე",
+    "ზაზა ბორისოვი",
+    "ნანუკა დეკანოსიძე",
+    "ნიკოლოზ ცაავა",
+    "ირაკლი გერაძე",
+    "ნიკოლოზ ნადირაძე"
+]
+
+lucky_controllers = []
+def get_cont(ls):
+    new_cont = random.choice(ls)
+    ls.remove(new_cont)
+
+    lucky_controllers.append(new_cont)
+
+for _ in range(3):
+    wait_time = random.randint(1, 5)
+    time.sleep(wait_time) 
+    
+    get_cont(names)
+    print(f"\n{"-"*(len(lucky_controllers[-1])+16)}\n\nNew controller: {lucky_controllers[-1]}\n\n{"-"*(len(lucky_controllers[-1])+16)}\n")
